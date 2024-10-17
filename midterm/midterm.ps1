cad0e36c4987dd5897d8bb876ce514dc911d870a
Logan <logan.clark@mymail.champlain.edu>
2024-10-17 14:39:00 -0400

    Format code

--- midterm/midterm.ps1
+++ midterm/midterm.ps1
@@ -1,8 +1,6 @@
 function getTable() {
     $scrapped = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.5/IOC.html
-
     $page = $scrapped.ParsedHtml.body.getElementsByTagName("tr")
-
     $FullTable = @()
 
     for ($i = 1; $i -lt $page.length; $i++) {
@@ -21,6 +19,7 @@ getTable
 function ApacheLogs() {
     $visits = Get-Content -Path C:\users\champuser\Desktop\CSI-230\midterm\access.log
     $tableRecords = @()
+
     for ($i = 0; $i -lt $visits.Count; $i++) {
         $words = $visits[$i].Split(" ")
         $tableRecords += [pscustomobject]@{
@@ -41,6 +40,7 @@ ApacheLogs | Format-Table
 function Challenge3($logFile, $indicator) {
     $visits = Get-Content -Path C:\users\champuser\Desktop\CSI-230\midterm\$logFile
     $tableRecords = @()
+    
     for ($i = 0; $i -lt $visits.Count; $i++) {
         $words = $visits[$i].Split(" ")
         $tableRecords += [pscustomobject]@{
