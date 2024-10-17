dd90b7b220c6bf3306d57063c01149466d89a711
Logan <logan.clark@mymail.champlain.edu>
2024-10-17 14:29:32 -0400

    Midterm

--- midterm/midterm.ps1
+++ midterm/midterm.ps1
@@ -15,3 +15,44 @@ for($i=1; $i -lt $page.length; $i++) {
 return $FullTable
 }
 getTable
+
+
+function ApacheLogs() {
+$visits = Get-Content -Path C:\users\champuser\Desktop\CSI-230\midterm\access.log
+$tableRecords = @()
+for($i = 0; $i -lt $visits.Count; $i++) {
+$words = $visits[$i].Split(" ")
+$tableRecords += [pscustomobject]@{"IP" = $words[0];`
+                                    "Time" = $words[3].Trim('['); `
+                                    "Method" = $words[5].Trim('"'); `
+                                    "Page" = $words[6]; `
+                                    "Protocol" = $words[7]; `
+                                    "Response" = $words[8]; `
+                                    "Referrer" = $words[10];
+                                }
+}
+return $tableRecords
+}
+
+ApacheLogs | Format-Table
+
+
+function Challenge3($logFile, $indicator) {
+        $visits = Get-Content -Path C:\users\champuser\Desktop\CSI-230\midterm\$logFile
+        $tableRecords = @()
+        for($i = 0; $i -lt $visits.Count; $i++) {
+        $words = $visits[$i].Split(" ")
+        $tableRecords += [pscustomobject]@{"IP" = $words[0];`
+                                            "Time" = $words[3].Trim('['); `
+                                            "Method" = $words[5].Trim('"'); `
+                                            "Page" = $words[6]; `
+                                            "Protocol" = $words[7]; `
+                                            "Response" = $words[8]; `
+                                            "Referrer" = $words[10];
+                                        }
+        }
+        # Modified from https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch07s13.html
+        return $tableRecords | Where-Object {$_.Page -match ".$indicator.*\?([^#]+)"} | Format-Table
+        }
+
+Challenge3 access.log index
