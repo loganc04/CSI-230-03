10f9c5079e78ac94b1d6de6c85a1929180256d57
Logan <logan.clark@mymail.champlain.edu>
2024-11-13 16:31:05 -0500

    Work on Linux Bash Apache Log Menu. Still need to fix frequentVisitors function

--- week12/apacheLogMenu.bash
+++ week12/apacheLogMenu.bash
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-logFile="/var/log/apache2/access.log.1"
+logFile="/var/log/apache2/access.log"
 
 function displayAllLogs(){
 	cat "$logFile"
@@ -10,8 +10,9 @@ function displayOnlyIPs(){
         cat "$logFile" | cut -d ' ' -f 1 | sort -n | uniq -c
 }
 
-# function: displayOnlyPages:
-# like displayOnlyIPs - but only pages
+function displayOnlyPages() {
+	cat "$logFile" | cut -d ' ' -f 7 | tr -d '['  | sort -n | uniq -c 
+}
 
 function histogram(){
 
@@ -41,6 +42,25 @@ function histogram(){
 # the output should be almost identical to histogram
 # only with daily number of visits that are greater than 10 
 
+function frequentVisitors() {
+	local visitsPerDay=$(cat "$logFile" | cut -d " " -f 4,1 | tr -d '[' | sort | uniq)
+	:> freq.txt
+	
+	echo "$visitsPerDay" | while read -r line;
+	do
+		local withoutHours=$(echo "$line" | cut -d " " -f 2 | cut -d ":" -f 1)
+		local ip=$(echo "$line" | cut -d " " -f 1)
+		local arranged=$(echo "$ip $withoutHours")
+			echo "$arranged" >> freq.txt
+	done
+
+	local sorted=$(cat "freq.txt" | sort -n | uniq -c)
+	:> freq.txt
+	echo "$sorted" >> freq.txt
+	cat "freq.txt"
+}
+
+
 # function: suspiciousVisitors
 # Manually make a list of indicators of attack (ioc.txt)
 # filter the records with this indicators of attack
@@ -50,16 +70,20 @@ function histogram(){
 # Keep in mind that I have selected long way of doing things to 
 # demonstrate loops, functions, etc. If you can do things simpler,
 # it is welcomed.
+function suspiciousVisitors() {
+	tail "$logFile" | egrep -i -f /home/champuser/CSI-230/week12/IOC.txt | cut -d ' ' -f1 |  uniq -c
+}
+
 
 while :
 do
-	echo "PLease select an option:"
+	echo "Please select an option:"
 	echo "[1] Display all Logs"
 	echo "[2] Display only IPS"
-	# Display only pages visited
+	echo "[3] Display only pages visited"
 	echo "[4] Histogram"
-	# Frequent visitors
-	# Suspicious visitors
+	echo "[5] Frequent Visitors"
+	echo "[6] Suspicious Visitors"
 	echo "[7] Quit"
 
 	read userInput
@@ -78,12 +102,24 @@ do
 		displayOnlyIPs
 
 	# Display only pages visited
+	elif [[ "$userInput" == "3" ]]; then
+		echo "Displaying visited pages:"
+		displayOnlyPages
 
 	elif [[ "$userInput" == "4" ]]; then
 		echo "Histogram:"
 		histogram
 
         # Display frequent visitors
+
+        elif [[ "$userInput" == "5" ]]; then
+        	echo "Frequent Visitors:"
+        	frequentVisitors
+
+
+        elif [[ "$userInput" == "6" ]]; then
+        	echo "Suspicious Visitors:"
+        	suspiciousVisitors
 	# Display suspicious visitors
 	# Display a message, if an invalid input is given
 	fi
